b2ade4c2446248eaf6e59073b8a0e66ece8bed46
Arthur Vieira <arthurvieira12365@gmail.com>
2026-04-01 20:24:01 -0300

    Melhorando o desafio e colocando coisas novas>

--- exercicio_facu/jogos/PPT.py
+++ exercicio_facu/jogos/PPT.py
@@ -2,19 +2,35 @@ from random import *
 
 print("0 = Pedra, 1 = Tesoura, 2 = Papel")
 
-player= int(input("Digite um numero de 0 a 2: "))
+vitorias_humano = 0
+vitorias_computador = 0
 
-computador = randint(0,2)
-
-if player > 2 or player < 0:
-    print("Número invalido")
+quantidades = int(input("Digite quantas vezes você quer jogar? "))
+if quantidades < 1 :
+    print("Quantidade inválida")
 else:
-    if player == computador:
-        condicao = "Empate"
-    else:
-        if (player == 0 and computador == 1) or (player == 1 and computador == 2) or (player == 2 and computador == 0) :
-            condicao = "Você Venceu!"
+    for i in range(quantidades):
+        player= int(input("Digite um numero de 0 a 2: "))
+
+        computador = randint(0,2)
+
+        if player > 2 or player < 0:
+            print("Número invalido")
         else:
-            condicao = "Mais sorte da proxima vez!"
-    print("O computador escolheu: " ,computador)
-    print(condicao)
+            if player == computador:
+                condicao = "Empate"
+            else:
+                if (player == 0 and computador == 1) or (player == 1 and computador == 2) or (player == 2 and computador == 0) :
+                    condicao = "Você Venceu!"
+                    vitorias_humano += 1
+                else:
+                    condicao = "Mais sorte da proxima vez!"
+                    vitorias_computador += 1
+            print("O computador escolheu: " ,computador)
+            print(condicao)
+    empate = quantidades - (vitorias_humano + vitorias_computador)
+    print(f"O jogador ganhou no total: {vitorias_humano} , O computador ganhou no total: {vitorias_computador} ")
+    if empate > 0:
+        print(f"A quantidade de empates foi de {empate}")
+    else:
+        print("Não teve empates")
