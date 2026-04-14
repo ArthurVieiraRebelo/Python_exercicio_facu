439fbc9d11804c7af86197a4eff3512fda324f0b
Arthur Vieira <arthurvieira12365@gmail.com>
2026-04-13 22:17:02 -0300

    arrumando o exercicio 4

--- exercicio_facu/Algoritmos-lista3/exercicio4.py
+++ exercicio_facu/Algoritmos-lista3/exercicio4.py
@@ -1,8 +1,9 @@
+salario_total = 0
 filhos_total = 0
 pessoas_ate_1000 = 0
 quantidade_de_pessoas = 0
 salario = float(input(f"Digite o salário da pessoa: "))
-salario_total = salario
+maior_salario = salario
 while salario >= 0:
     quantidade_de_pessoas += 1
     filhos = int(input(f"Quantos filhos a pessoa tem? "))
@@ -14,11 +15,11 @@ while salario >= 0:
     filhos_total += filhos
     salario = float(input(f"Digite o salário da pessoa: "))
 
-media_salarial = salario_total / quantidade_de_pessoas
-media_filhos = filhos_total / quantidade_de_pessoas
-percentual_ate_1000 = (pessoas_ate_1000 / quantidade_de_pessoas) * 100
-
 if quantidade_de_pessoas > 0:
+    media_salarial = salario_total / quantidade_de_pessoas
+    media_filhos = filhos_total / quantidade_de_pessoas
+    percentual_ate_1000 = (pessoas_ate_1000 / quantidade_de_pessoas) * 100
+
     print(f"A média dos salários é {media_salarial:.2f}")
     print(f"A média do número de filhos são {media_filhos:.2f}")
     print(f"O maior salário é de R${maior_salario:.2f}")
