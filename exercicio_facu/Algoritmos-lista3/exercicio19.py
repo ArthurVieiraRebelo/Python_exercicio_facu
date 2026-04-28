b1737b10b7566004cf9554db3597fb69fb31a105
Arthur Vieira <arthurvieira12365@gmail.com>
2026-04-27 21:45:58 -0300

    Arrumando o 19

--- exercicio_facu/Algoritmos-lista3/exercicio19.py
+++ exercicio_facu/Algoritmos-lista3/exercicio19.py
@@ -1,8 +1,14 @@
-numero = int(input("Digite um número: "))
-fatorial = 1
-soma = 0
-for i in range(1, numero + 1):
-    fatorial *= i
-    divisor = 1 / fatorial
-    soma += divisor
-print(f"A soma é {soma:.2f}")
+# 19.	Escrever um algoritmo que leia um valor
+# N inteiro e positivo e que calcula o valor de
+# E. Imprime o resultado de E ao final.
+# E = 1 + 1 / 1! + 1 / 2! + 1 / 3! + 1 / N!
+
+e = 1
+n = int(input("Digite um numero: "))
+if n > 0:
+    for i in range(1, n + 1):
+        fat = 1
+        for j in range(1, i + 1):
+            fat = fat * j
+        e = e + (1 / fat)
+    print(f"E = {e:.2f}")
