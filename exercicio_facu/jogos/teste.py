9b06ea459c611cbac616a60c85ad65e2b38f136c
Arthur Vieira <arthurvieira12365@gmail.com>
2026-06-26 12:43:04 -0300

    .

--- exercicio_facu/jogos/teste.py
+++ exercicio_facu/jogos/teste.py
@@ -1,84 +1,184 @@
+import random
 
-def perguntar_jogada():
-    jogada1 = int(input("Digite em qual linha você quer jogar: "))
-    jogada2 = int(input("Digite em qual coluna você quer jogar: "))
-    return jogada1, jogada2
+def jogada_jogador():
+    linha = int(input("Digite a linha (0, 1 ou 2): "))
+    coluna = int(input("Digite a coluna (0, 1 ou 2): "))
+    if linha < 0 or linha > 2 or coluna < 0 or  coluna > 2:
+        while linha < 0 or linha > 2 or coluna < 0 or  coluna > 2:
+            print("Jogada inválida!")
+            linha = int(input("Digite a linha (0, 1 ou 2): "))
+            coluna = int(input("Digite a coluna (0, 1 ou 2): "))
+    else:
+        if not tabuleiro[linha][coluna]:
+            tabuleiro[linha][coluna] = simbolo_jogador
+            return linha, coluna
+        else:
+            print("Lugar já ocupado, tente novamente!")
+    return linha, coluna
 
-def mostra_tabuleiro(tabuleiro):
-    for linha in tabuleiro:
-        for elemento in linha:
-            print(elemento, end=" ")
+def jogada_maquina():
+
+
+    jogada = encontrar_jogada_vencedora(simbolo_maquina)
+    if jogada:
+        tabuleiro[jogada[0]][jogada[1]] = simbolo_maquina
+        return jogada[0], jogada[1]
+
+    jogada = encontrar_jogada_vencedora(simbolo_jogador)
+    if jogada:
+        tabuleiro[jogada[0]][jogada[1]] = simbolo_maquina
+        return jogada[0], jogada[1]
+
+    cantos = [[0,0], [2,2], [0,2], [2,0]]
+    random.shuffle(cantos)
+    for canto in cantos:
+        if not tabuleiro[canto[0]][canto[1]]:
+            tabuleiro[canto[0]][canto[1]] = simbolo_maquina
+            return canto[0], canto[1]
+
+    for i in range(3):
+        for j in range(3):
+            if tabuleiro[i][j] == 0:
+                tabuleiro[i][j] = simbolo_maquina
+                return i, j
+
+def encontrar_jogada_vencedora(jogador):
+    for i in range(3):
+        contador_jogador = 0
+        contador_vazio = 0
+        for j in range(3):
+            if tabuleiro[i][j] == jogador:
+                contador_jogador += 1
+            if tabuleiro[i][j] == 0:
+                contador_vazio += 1
+        if contador_jogador == 2 and contador_vazio == 1:
+            for j in range(3):
+                if tabuleiro[i][j] == 0:
+                    return i, j
+
+        contador_jogador = 0
+        contador_vazio = 0
+        for j in range(3):
+            if tabuleiro[j][i] == jogador:
+                contador_jogador += 1
+            if tabuleiro[j][i] == 0:
+                contador_vazio += 1
+        if contador_jogador == 2 and contador_vazio == 1:
+            for j in range(3):
+                if tabuleiro[j][i] == 0:
+                    return j, i
+
+    contador_jogador = 0
+    contador_vazio = 0
+    for i in range(3):
+        if tabuleiro[i][i] == jogador:
+            contador_jogador += 1
+        if tabuleiro[i][i] == 0:
+            contador_vazio += 1
+    if contador_jogador == 2 and contador_vazio == 1:
+        for i in range(3):
+            if tabuleiro[i][i] == 0:
+                return i, i
+
+    contador_jogador = 0
+    contador_vazio = 0
+    for i in range(3):
+        if tabuleiro[i][2-i] == jogador:
+            contador_jogador += 1
+        if tabuleiro[i][2-i] == 0:
+            contador_vazio += 1
+    if contador_jogador == 2 and contador_vazio == 1:
+        for i in range(3):
+            if tabuleiro[i][2-i] == 0:
+                return i, 2-i
+
+def verificar_possibilidade(linha, coluna, jogador):
+    if tabuleiro[linha][0] == tabuleiro[linha][1] == tabuleiro[linha][2] == jogador:
+        return True
+    if tabuleiro[0][coluna] == tabuleiro[1][coluna] == tabuleiro[2][coluna] == jogador:
+        return True
+    if linha == coluna:
+        if tabuleiro[0][0] == tabuleiro[1][1] == tabuleiro[2][2] == jogador:
+            return True
+    if linha + coluna == 2:
+        if tabuleiro[0][2] == tabuleiro[1][1] == tabuleiro[2][0] == jogador:
+            return True
+    return False
+
+def verificar_velha():
+    for i in range(3):
+        for j in range(3):
+            if tabuleiro[i][j] == 0:
+                return False
+    return True
+
+def imprimir_tabuleiro():
+    for i in range(3):
+        for j in range(3):
+            print(simbolos[tabuleiro[i][j]], end=" ")
         print()
 
-def jogada_humano(jogada1, jogada2):
-    if tabuleiro[jogada1][jogada2] != "x":
-        tabuleiro[jogada1][jogada2] = "o"
+def resetar_tabuleiro():
+    for i in range(3):
+        for j in range(3):
+            tabuleiro[i][j] = 0
+
+placar_vitoria_j = 0
+placar_vitoria_m = 0
+placar_velha = 0
+jogar_de_novo = True
+
+tabuleiro = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+
+while jogar_de_novo == True :
+    vencedor = 0
+    turno = random.randint(1,2)
+    resetar_tabuleiro()
+
+    if turno == 1:
+        print("Você começa! Você é o X")
+        simbolo_jogador = 1
+        simbolo_maquina = 2
+        simbolos = {0: ".", 1: "X", 2: "O"}
     else:
-        print("Esse já está ocupado, escolha outro!")
-
-    return tabuleiro
-
-def verificar_linha(tabuleiro):
-    ganhar = 0
-    linha_vazia = 0
-    coluna_vazia = 0
-    for i in range(0,3) :
-        ganhar = 0
-        for j in range(0, 3):
-            if tabuleiro[i][j] == "o":
-                ganhar += 1
-            else:
-                if tabuleiro[i][j] == "*":
-                    linha_vazia = i
-                    coluna_vazia = j
-        if ganhar == 2:
-            tabuleiro[linha_vazia][coluna_vazia] = "x"
-    return tabuleiro
-
-def coluna_0_de_cima_baixo(tabuleiro):
-    linha_vazia = 0
-    coluna_vazia = 0
-    coluna = -1
-    coluna_fim = 0
-    for a in range(0,3):
-        ganhar = 0
-        coluna += 1
-        coluna_fim += 1
-        for i in range(0,3):
-            for j in range(coluna, coluna_fim):
-                if tabuleiro[i][j] == "o":
-                    ganhar += 1
-                else:
-                    if tabuleiro[i][j] == "*":
-                        linha_vazia = i
-                        coluna_vazia = j
-        if ganhar == 2:
-            tabuleiro[linha_vazia][coluna_vazia] = "x"
-    return tabuleiro
-
-def verificar_se_pode_ganhar_linha(tabuleiro):
-    ganhar = 0
-    linha_vazia = 0
-    coluna_vazia = 0
-    for i in range(0, 3):
-        ganhar = 0
-        for j in range(0, 3):
-            if tabuleiro[i][j] == "x":
-                ganhar += 1
-            else:
-                if tabuleiro[i][j] == "*":
-                    linha_vazia = i
-                    coluna_vazia = j
-        if ganhar == 2:
-            tabuleiro[linha_vazia][coluna_vazia] = "x"
+        print("Máquina começa! Você é o O")
+        simbolo_jogador = 2
+        simbolo_maquina = 1
+        simbolos = {0: ".", 1: "X", 2: "O"}
 
 
+    imprimir_tabuleiro()
 
-jogou = False
+    while vencedor == 0:
+        if turno == 1:
+            linha, coluna = jogada_jogador()
+            print("Sua jogada:")
+            imprimir_tabuleiro()
+            if verificar_possibilidade(linha, coluna, simbolo_jogador):
+                vencedor = 1
+                print("Você ganhou!")
+                placar_vitoria_j += 1
+            turno = 2
+        else:
+            print("Jogada da máquina:")
+            linha, coluna = jogada_maquina()
+            imprimir_tabuleiro()
+            if verificar_possibilidade(linha, coluna, simbolo_maquina):
+                vencedor = 2
+                print("Máquina ganhou!")
+                placar_vitoria_m += 1
+            turno = 1
 
-tabuleiro = [ ["x", "*" , "*"],
-              ["x", "*" , "x"],
-              ["*", "o", "o"] ]
-mostra_tabuleiro(tabuleiro)
-verificar_se_pode_ganhar_linha(tabuleiro)
-mostra_tabuleiro(tabuleiro)
+        if verificar_velha() and vencedor == 0:
+            vencedor = 3
+            print("Velha!")
+            placar_velha += 1
+    print()
+    print(f"Placar - Você: {placar_vitoria_j} | Máquina: {placar_vitoria_m} | Velhas: {placar_velha}")
+    resposta = input("Quer jogar de novo? (s/n): ")
+    if resposta != "s":
+        jogar_de_novo = False
